09e1ee2cb4936930dca654ae787b140bea57b9ff
Nilay Saxena <nilaysaxena94@gmail.com>
2025-04-20 19:31:52 +0530

    Updated Port

--- frontend/app.py
+++ frontend/app.py
@@ -20,4 +20,4 @@ def home():
     return render_template("index.html", fe_hostname=fe_hostname, be_hostname=be_hostname)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=8000)
